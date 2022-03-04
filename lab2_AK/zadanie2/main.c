0abed22afcd3da958bc703d4b7364a1a0796fad4
MichallBB <166235@student.uwm.edu.pl>
2022-03-04 12:45:59 +0100

    Create main.c

---
+++ lab2_AK/zadanie2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int liczba1=4;
+    int liczba2=4;
+    int liczba3=5;
+    float srednia=(liczba1+liczba2+liczba3)/3;
+    printf("%f",srednia);
+    return 0;
+}
