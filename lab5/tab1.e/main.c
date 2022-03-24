9326f97c374a19d8a0985f5f9355e54f45949e1f
olusk123 <100926461+olusk123@users.noreply.github.com>
2022-03-24 22:50:37 +0100

    Create main.c

---
+++ lab5/tab1.e/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    const int N=1000;
+    int tab[N];
+    int n;
+    printf("Podaj liczbe elementow: " );
+    scanf("%d",&n);
+    if (n<=0||n>N)
+    {
+        printf("Liczba elementow musi byc z przedzialu [1, %d\n]", N);
+        return 1;
+    }
+    for (int i=0; i<n; i++)
+    {
+        tab[i]=rand()%100+1;
+         printf("[%d] = %d\n", i, tab[i]);
+    }
+    printf("e\n");
+    int maxi=tab[0];
+    int ile=0;
+    for (int i=1;i<n;i++)
+    {
+    	if(maxi<tab[i])
+    		maxi=tab[i];
+	}
+	for (int i=0;i<n;i++)
+	{
+		if (maxi==tab[i])
+			ile++;
+	}
+	printf("Najwiekszy: %d, ilosc wystapienia %d ", najwiekszy, ile);
+
+return 0;
