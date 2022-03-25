c14836103c0ee515e55f9b9b6856169598ee6747
olusk123 <100926461+olusk123@users.noreply.github.com>
2022-03-25 09:21:35 +0100

    Update main.c

--- lab5/zad3.2.12/main.c
+++ lab5/zad3.2.12/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int*alokacja(int n)
+int*alo(int m)
 {
     int*x=malloc(n*sizeof(double));
     return*x;
 }
 int main()
 {
-    int n=10;
-    printf("%p",alokacja(n));
+    int m=10;
+    printf("%p",alo(m));
     return 0;
 }
