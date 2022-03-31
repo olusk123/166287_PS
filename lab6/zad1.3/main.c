2126eefb6f55ac6eec15816cc54e8f5865fbd10d
olusk123 <100926461+olusk123@users.noreply.github.com>
2022-03-31 21:03:23 +0200

    Create main.c

---
+++ lab6/zad1.3/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    const int N=1000;
+    int prawy,lewy;
+    short tab[N];
+    int n;
+    printf("Podaj liczbe elementow: " );
+    scanf("%d",&n);
+    if (n<=0||n>N)
+    {
+        printf("Liczba elementow musi byc z przedzialu [1, %d\n]", N);
+        return 1;
+    }
+   short lim = 999;
+   srand(time(0));
+   for(short*i = tab; i < tab + n; ++i) {
+        *i = rand() % lim + 1;
+   }
+   for(short*i = tab; i < tab + n; ++i) {
+        printf("%5d ",*i);
+   }
+    printf("c:\n");
+    int tmp;
+    short *j=tab+prawy;
+    for(short *i=tab+lewy;i<tab+prawy;i++)
+    {
+        tmp = *i;
+        *i = *j;
+        *j = tmp;
+        prawy--;
+    }
+return 0;
+}
