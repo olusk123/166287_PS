8ffaf36f1842f1600443930b97ef7b137a877ee0
olusk123 <100926461+olusk123@users.noreply.github.com>
2022-04-21 18:59:23 +0200

    Create main.c

---
+++ lab8/5.2.6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void kopiujn(char * tab1[],char * tab2[],int n)
+{
+
+}
+int main()
+{
+    char tab1[20]="napis";
+    char tab2[20]="sipan";
+    int n=3;
+
+    return 0;
+}
