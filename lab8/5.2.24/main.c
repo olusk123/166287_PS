c690c63407a100c70b8117fbac4b8988134256c9
Aleksander Kubel <166287@student.uwm.edu.pl>
2022-04-22 11:11:06 +0200

    Update main.c

--- lab8/5.2.24/main.c
+++ lab8/5.2.24/main.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void kopiuj(const char * tab1[],char napis){
-    memcpy(tab1, napis, strlen(tab1)+1);
-}
-
-int main()
-{
-    char napis ="NAPIS";
-    char tab1[20];
-    strcpy(tab1,"jakisnapis");
-    printf("przed = %s\n", tab1);
-    kopiuj(tab1,napis);
-    printf("po = %s\n", tab1);
-    return 0;
-}
