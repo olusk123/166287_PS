8c8fff1af34048444eed5714f366d538f61bb53e
Aleksander Kubel <166287@student.uwm.edu.pl>
2022-04-22 11:27:59 +0200

    Create main.c

---
+++ lab8/lab8/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int tablica(int t[][100], int n)
+{
+    int suma=0;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<100;j++)
+            suma+=t[i][j];
+    return suma;
+}
+size_t strlen(const char*s);
+char *strcpy(char*t, const char*s);
+
+size_t strlen(const char*s)
+{
+    size_t len = 0;
+    while(*s++)
+        {
+            ++len;
+        }
+    return len;
+}
+
+char*strcpy(char *t,const char *s)
+{
+    char*p = t;
+    while((*t++ = *s++));
+    return p;
+}
+
+int main(void)
+{
+
+    char *s = "Oto rzek³ Mistrz Programista:\n\n\
+    \"Po trzech dniach bez programowania, \
+    ¿ycie staje siê pozbawione sensu.\"\n";
+    char *p = calloc(strlen(s) + 1, sizeof(char));
+    strcpy(p, s);
+    printf("%s\n", p);
+    free(p);
+    return 0;
+}
