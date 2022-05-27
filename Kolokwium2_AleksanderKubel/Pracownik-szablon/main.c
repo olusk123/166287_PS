0f55a0e9e19b95caa343c6cf0f6fe0ca561d2b43
Aleksander Kubel <166287@student.uwm.edu.pl>
2022-05-27 12:39:52 +0200

    Create main.c

---
+++ Kolokwium2_AleksanderKubel/Pracownik-szablon/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "nauczyciel.h"
+#include <string.h>
+
+bool czyPrawidlowaPensja (double pensja);
+bool czyPrawidlowePensum (int pensum);
+
+
+int main(int argc, char *argv[])
+{   
+    // 1) Poproś użytkownika o wpisanie danych nauczyciela. Zabezpiecz dane! Zakładamy, że nauczyciel moze miec pensum z zakresu 120 do 360h, a pensja minimalna powinna być nie mniejsza niz 3010 zl . Napisz odpowiednie funkcje sprawdzające poprawność danych.
+    
+     // 2) Zainicjalizuj strukturę o nazwie prac1 wprowadzonymi danymi.
+    
+    // 3) Stwórz strukturę o nazwie prac2 kopiując obiekt prac1.
+    
+    // 4) Stwórz strukturę o nazwie prac3 kopiując obiekt prac1.
+    
+    // 5) Zmień nazwisko nauczyciela prac1 na Kowalski
+    
+    // 6) Zmień nazwisko nauczyciela prac2 na Nowak
+    
+    // 7) Zmień nazwisko nauczyciela prac3 na Nowakowski
+    
+    // 8) Podwyższ pensum nauczyciela prac2 o 100 godzin
+    
+    // 9) Zmień czyDyplomowany nauczyciela prac3 na true
+    
+    // 10) Podnieś pensję nauczyciela prac1 o inflację 12,3%
+    
+    // 11) Wypisz informacje o obiektach prac1, prac2, prac3
+    
+    // 12) stwórz tablicę o nazwie tab trzech Nauczcieli
+    
+    // 13) W pętli for do przechodzenia tablicy zamiast indeksów należy użyć wskaźników, a każdy operator indeksowania należy zastapić operatorem dereferencji!! 
+    // Przejdź tablicę i wypełnij ją nauczycielami uzywając odpowiedniej funkcji.
+
+    
+    // 14) W pętli for do przechodzenia tablicy zamiast indeksów należy użyć wskaźników, a każdy operator indeksowania należy zastapić operatorem dereferencji!! 
+    // Wypisz informacje o wszystkich obiektach typu Nauczyciel znajdujących sie w tablicy tab
+}
+
