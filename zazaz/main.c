815ad57eca2e8a570509184b263c272518c457d5
olusk123 <100926461+olusk123@users.noreply.github.com>
2023-02-12 18:13:37 +0100

    Update main.c

--- zazaz/main.c
+++ zazaz/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "LineSegment.h"
+#include "namedpoint.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    LineSegment odc1 = makeLineSegment(2,3,4,5);
-    show(&odc1);
+    NamedPoint tawerna = makeNamedPoint(3, 5, "Pod Mocnym Aniołem");
+    show(&tawerna);
+    printf("\n");
+    move(&tawerna, 1, -1);
+    show(&tawerna);
+    printf("\n");
+    NamedPoint centre = makeNamedPoint(0, 0, "Ratusz");
+    printf("Odległość tawerny od centrum: %f\n", distance(&tawerna, &centre));
     return 0;
 }
